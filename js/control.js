5cf94790393ed69000119c14280b9246146d07c8
kamelot88 <vasilchenkoov1988@gmail.com>
2020-07-01 19:22:25 +0300

    start restaling taxi

---
+++ js/control.js
@@ -0,0 +1,389 @@
+// Начало_Какая-то хрень, без которой Edg ругаеться
+[
+  {
+    type: "emulated-device",
+    device: {
+      "show-by-default": true,
+      title: "Microsoft Lumia 650",
+      screen: {
+        horizontal: {
+          width: 640,
+          height: 370
+        },
+        "device-pixel-ratio": 2,
+        vertical: {
+          width: 360,
+          height: 640
+        }
+      },
+      capabilities: ["touch", "mobile", "geolocation"],
+      "user-agent":
+        "Mozilla/5.0 (Windows Phone 10.0; Android 6.0.1; Microsoft; Lumia 650) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Mobile Safari/537.36 Edge/15.15063",
+      type: "phone",
+      modes: []
+    }
+  },
+  {
+    type: "emulated-device",
+    device: {
+      "show-by-default": true,
+      "select-by-default": true,
+      title: "Microsoft Lumia 950",
+      screen: {
+        horizontal: {
+          width: 640,
+          height: 360
+        },
+        "device-pixel-ratio": 4,
+        vertical: {
+          width: 360,
+          height: 640
+        }
+      },
+      capabilities: ["touch", "mobile", "geolocation"],
+      "user-agent":
+        "Mozilla/5.0 (Windows Phone 10.0; Android 6.0.1; Microsoft; Lumia 950) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Mobile Safari/537.36 Edge/15.15063",
+      type: "phone",
+      modes: []
+    }
+  },
+  {
+    type: "emulated-device",
+    device: {
+      "show-by-default": true,
+      title: "Microsoft Lumia 950XL",
+      screen: {
+        horizontal: {
+          width: 640,
+          height: 360
+        },
+        "device-pixel-ratio": 4,
+        vertical: {
+          width: 360,
+          height: 640
+        }
+      },
+      capabilities: ["touch", "mobile", "geolocation"],
+      "user-agent":
+        "Mozilla/5.0 (Windows Phone 10.0; Android 6.0.1; Microsoft; Lumia 950XL) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Mobile Safari/537.36 Edge/15.15063",
+      type: "phone",
+      modes: []
+    }
+  },
+  {
+    type: "emulated-device",
+    device: {
+      "show-by-default": true,
+      title: "Surface Book",
+      screen: {
+        horizontal: {
+          width: 640,
+          height: 360
+        },
+        "device-pixel-ratio": 4,
+        vertical: {
+          width: 360,
+          height: 640
+        }
+      },
+      capabilities: ["touch", "geolocation"],
+      "user-agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063",
+      type: "desktop",
+      modes: []
+    }
+  },
+  {
+    type: "emulated-device",
+    device: {
+      "show-by-default": true,
+      title: "Surface Pro 4",
+      screen: {
+        horizontal: {
+          width: 640,
+          height: 360
+        },
+        "device-pixel-ratio": 4,
+        vertical: {
+          width: 360,
+          height: 640
+        }
+      },
+      capabilities: ["touch", "geolocation"],
+      "user-agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063",
+      type: "desktop",
+      modes: []
+    }
+  },
+  {
+    type: "emulated-device",
+    order: 50,
+    device: {
+      "show-by-default": true,
+      title: "iPhone 7",
+      screen: {
+        horizontal: {
+          width: 667,
+          height: 375
+        },
+        "device-pixel-ratio": 2,
+        vertical: {
+          width: 375,
+          height: 667
+        }
+      },
+      capabilities: ["touch", "mobile"],
+      "user-agent":
+        "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1",
+      type: "phone",
+      modes: []
+    }
+  },
+  {
+    type: "emulated-device",
+    order: 60,
+    device: {
+      "show-by-default": true,
+      title: "iPhone 7 Plus",
+      screen: {
+        horizontal: {
+          width: 736,
+          height: 414
+        },
+        "device-pixel-ratio": 3,
+        vertical: {
+          width: 414,
+          height: 736
+        }
+      },
+      capabilities: ["touch", "mobile"],
+      "user-agent":
+        "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1",
+      type: "phone",
+      modes: []
+    }
+  },
+  {
+    type: "emulated-device",
+    device: {
+      "show-by-default": false,
+      title: "iPad Mini",
+      screen: {
+        horizontal: {
+          width: 1024,
+          height: 768
+        },
+        "device-pixel-ratio": 2,
+        vertical: {
+          width: 768,
+          height: 1024
+        }
+      },
+      capabilities: ["touch", "mobile"],
+      "user-agent":
+        "Mozilla/5.0 (iPad; CPU OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1",
+      type: "tablet",
+      modes: []
+    }
+  },
+  {
+    type: "emulated-device",
+    order: 70,
+    device: {
+      "show-by-default": true,
+      title: "iPad",
+      screen: {
+        horizontal: {
+          width: 1024,
+          height: 768
+        },
+        "device-pixel-ratio": 2,
+        vertical: {
+          width: 768,
+          height: 1024
+        }
+      },
+      capabilities: ["touch", "mobile"],
+      "user-agent":
+        "Mozilla/5.0 (iPad; CPU OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1",
+      type: "tablet",
+      modes: []
+    }
+  },
+  {
+    type: "emulated-device",
+    device: {
+      title: "Nexus 5",
+      type: "phone",
+      "user-agent":
+        "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.23 Mobile Safari/537.36",
+      capabilities: ["touch", "mobile"],
+      "show-by-default": false,
+      screen: {
+        "device-pixel-ratio": 3,
+        vertical: {
+          width: 360,
+          height: 640
+        },
+        horizontal: {
+          width: 640,
+          height: 360
+        }
+      },
+      modes: []
+    }
+  },
+  {
+    type: "emulated-device",
+    order: 20,
+    device: {
+      title: "Nexus 5X",
+      type: "phone",
+      "user-agent":
+        "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Mobile Safari/537.36",
+      capabilities: ["touch", "mobile"],
+      "show-by-default": true,
+      screen: {
+        "device-pixel-ratio": 2.625,
+        vertical: {
+          width: 411,
+          height: 731
+        },
+        horizontal: {
+          width: 731,
+          height: 411
+        }
+      },
+      modes: []
+    }
+  },
+  {
+    type: "emulated-device",
+    order: 30,
+    device: {
+      "show-by-default": true,
+      title: "Nexus 6P",
+      screen: {
+        horizontal: {
+          width: 773,
+          height: 435
+        },
+        "device-pixel-ratio": 3.3,
+        vertical: {
+          width: 435,
+          height: 773
+        }
+      },
+      capabilities: ["touch", "mobile"],
+      "user-agent":
+        "Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Mobile Safari/537.36",
+      type: "phone",
+      modes: []
+    }
+  }
+];
+// Конец_Какая-то хрень, без которой Edg ругаеться
+
+$(function () {
+  "use strict";
+
+  // Начало функции для плавного перехода по якорным ссылкам на странице
+  $('a[href^="#"]').click(function () {
+    let target = $(this).attr("href");
+    $("html, body").animate(
+      {
+        scrollTop: $(target).offset().top
+      },
+      800
+    );
+  });
+  // Конец функции для плавного перехода по якорным ссылкам на странице
+
+  // Начало функции для закрепления кнопок header при скроулинге
+  var offset = $("#top_header").offset();
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > offset.top) {
+      $("#top_header").addClass("FixedNav");
+    } else {
+      $("#top_header").removeClass("FixedNav");
+    }
+  });
+
+  // 	Для отображения панели с кнопками при скроулинге необходимо прописать стили в css для класса 'FixedNav':
+  // 	.FixedNav {
+  // 	position: fixed;
+  // 	top: 0;
+  // 	opacity: 0.7;
+  // 	background-color: #fff;
+  // 	left: 0;
+  // 	color: #323232;
+  // 	width: 100%;
+  // 	height: 10vh;
+  // 	z-index: 10;
+  // 	transition: 0.25s ease-in-out;
+  // }
+  // .FixedNav:hover {
+  // 	opacity: 1;
+  // }
+  // .FixedNav a {
+  // 	color: #323232 !important;
+  // }
+  // .FixedNav nav {
+  // 	height: inherit;
+  // }
+  // Конец функции для закрепления кнопок header при скроулинге
+
+  // Начало функции слайдера для мобильных устройств
+
+  if (window.innerWidth > 1024) {
+    $('#slide').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
+  } else {
+    $('#slide').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
+  }
+
+  // Для родительского блока элементов слайдера необходимо добавить id="slide".
+  // Данный скрипт описывает условие количесвта элементов слайда на экране для мобильного
+  //  устройства (ширина экрана менее 1024px) и для ПК (ширина экрана, более 1024px)
+  // 	Конец функции слайдера для мобильных устройств
+
+  // Начало_Кнопка добавляет блок текста к описанию
+
+  $("#view_all_text").click(function () {
+    let content = '<div class="third main_flex flex__jcontent_around"><div class="container5 main_flex__nowrap flex__align-items_start" ><div class="i05"><img src="img/usd_dollar_3313.png" alt="" /></div><div class="text_container"><h3>экономим ваши деньги</h3><p>С целью экономии Ваших средств, с 15.01.2020г. мы значительно снизили тарифы</p></div></div ><div class="container6 main_flex__nowrap flex__align-items_start"><div class="i06"><img src="img/cooperation4x_86995.png" alt="" /></div><div class="text_container"><h3>Сотрудничество</h3><p>Предлагаем взаимовыгодное сотрудничество корпоративным клиентам</p></div></div></div ><div class="fourth main_flex flex__jcontent_around"><div class="container7 main_flex__nowrap flex__align-items_start"><div class="i07"><img src="img/-work_90306.png" alt="" /></div><div class="text_container"><h3>Работа у нас</h3><p>Выгодное условие сотрудничества для водителей на собственном автомобиле</p></div></div><div class="container8 main_flex__nowrap flex__align-items_start"><div class="i0"><img src="" alt="" /></div><div class="text_container"><h3></h3><p></p></div></div></div>';
+    $(".content").append(content);
+    $(this).remove();
+  });
+  // Конец_Кнопка добавляет блок текста к описанию
+
+  // Начало_ добавляет больше телефонов в блок Yellow phone
+
+  // $("#more_phone").click(function () {
+  //   let content =
+  //     '<div class="more_phone main_flex_column"><a href="tel:+380500182020" class="white">+38 050 018 20 20</a><a href="tel:+380980182020" class="white">+38 098 018 20 20</a><a href="tel:+380930182020" class="white">+38 093 018 20 20</a></div>';
+  //   $(".yellow_phone_title").append(content);
+  //   $(this).remove();
+  // });
+  // Конец_добавляет больше телефонов в блок Yellow phone
+
+  // Начало_функция открытия/закрытия формы
+  $("#modal_box, #black_fill").hide();
+  $(".black_btn").click(function () {
+    $("#modal_box, #black_fill").show();
+  });
+  $("#close_modal, #black_fill").click(function () {
+    $("#modal_box, #black_fill").hide();
+  });
+  // Конец_функция открытия/закрытия формы
+});
